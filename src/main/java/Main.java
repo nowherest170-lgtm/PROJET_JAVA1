af31084851861556693efd6e19bf77849ffc5bb7
nowherest <nowherest170@gmail.com>
2026-02-07 14:48:28 +0200

    productionManager operatorManager

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import manager.ProductionManager;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductionManager pm = new ProductionManager();
+        pm.saisirProduction(); // lance la saisie
+    }
+}
